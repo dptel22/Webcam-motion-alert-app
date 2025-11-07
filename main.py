babb1bfcbc098e4a58ecd9df5d5da0f98f4a848e
dhruv <dhruvpt933@gmail.com>
2025-11-07 18:08:54 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,45 @@
+from emailing import send_email
+import cv2
+import time
+
+video = cv2.VideoCapture(0)
+time.sleep(1)
+
+first_frame = None
+while True:
+    check, frame  = video.read()
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray_gau_frame = cv2.GaussianBlur(gray_frame, (21, 21), 0)
+
+
+    if first_frame is None:
+        first_frame = gray_gau_frame
+
+    delta_frame = cv2.absdiff(first_frame, gray_gau_frame)
+
+    thresh_frame = cv2.threshold(delta_frame, 60, 255, cv2.THRESH_BINARY)[1]
+    div_frame = cv2.dilate(thresh_frame, None, iterations=2)
+    #cv2.imshow("My Video", div_frame)
+
+    contours, check  = cv2.findContours(div_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    for contour in contours:
+        if cv2.contourArea(contour) < 5000:
+            continue
+
+        x, y, w, h = cv2.boundingRect(contour)
+        rectangle = cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
+
+        if rectangle.any():
+            send_email()
+
+
+    cv2.imshow("Video", frame)
+    key = cv2.waitKey(1)
+
+    if key == ord("q"):
+        break
+
+
+video.release()
+
